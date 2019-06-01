977e575f77b02b5132ebcdfaba6584c13a605bd1
Thais Lasso Terense <ylbulbasaur@Andersons-MacBook-Pro.local>
2019-06-01 09:24:42 -0300

    v3

--- scripts/main.js
+++ scripts/main.js
@@ -18,8 +18,19 @@ var loginData = [
         {
             usuario: "Naiara",
             senha: "Alflen"
+        },
+        {
+            usuario: "Avaliador",
+            senha: "Avaliador"
         }
     ];
+
+
+//login apertando a tecla enter
+document.addEventListener('keydown', event => {
+    const code = (event.keyCode ? event.keyCode : event.which);
+    if(code == 13 && document.getElementById("usuario").value != "" && document.getElementById("senha").value != "") validateUser();
+});
 
 function validateUser() {
     var usuario = document.getElementById("usuario").value;
@@ -35,6 +46,15 @@ function validateUser() {
     }
     alert("Usuário ou senha incorretos");
     return false;
+    }
+
+
+function confirmaCadastro() {
+    confirm("Deseja finalizar o cadastramento?");
+}
+
+function confirmaCadastro() {
+    confirm("Deseja submeter a avaliação?");
 }
 
 function sidenav_toggle(){
@@ -48,6 +68,47 @@ function sidenav_toggle(){
     }
 }
 
-function avaliar(){
+//avaliar
+function avaliar(text){
     window.location = "feedback.html";
 }
+
+//collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+//feedback checkbox mostrar todas
+var checkb = document.getElementById("expand_all");
+checkb.addEventListener("click", function (){
+    var disp = "none";
+    var content;
+    if(checkb.checked){
+        disp = "block";
+    }
+    for(i = 0; i < coll.length; i++){
+        coll[i].classList.toggle("active");
+        content = coll[i].nextElementSibling;
+        content.style.display = disp;
+    }
+});
+
+
+/*/relatorio checkbox selecionar todas
+var trs = document.getElementsByTagName("tr");
+var select_all = document.getElementById("show_all_reports");
+select_all.addEventListener("click", function (){
+    for(i = 0; i < trs.length; i++){
+        trs[i].
+    }
+}*/
